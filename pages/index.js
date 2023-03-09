9575936b36c1eb84850d88cd8f885fd7ea21d9d9
ArianeDahl <ariane.dahl@googlemail.com>
2023-03-09 12:34:19 +0100

    add update data.js, delete buttons, add links instead, add vegetable and fruit page

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,60 @@
-import FruitButton from "@/components/FruitButton";
-import VeggieButton from "@/components/VeggieButton";
 import Header from "@/components/Header";
 import styled from "styled-components";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <div>
-        <Header />
-        <h2>Seasonal dishes made easy!</h2>
-        <p>See what fruits & vegetables are in season this month:</p>
-        <FruitButton />
-        <VeggieButton />
-      </div>
+      <MainContainer>
+        <>
+          <Header />
+          <h2>Seasonal dishes made easy!</h2>
+          <p>See what fruits & vegetables are in season this month:</p>
+          <LinkWrapper>
+            <Link href="/fruits">
+              <StyledLink>Fruits</StyledLink>
+            </Link>
+            <Link href="/vegetables">
+              <StyledLink>Veggies</StyledLink>
+            </Link>
+          </LinkWrapper>
+        </>
+      </MainContainer>
     </main>
   );
 }
+
+const MainContainer = styled.main`
+  background-color: #f2f2f2;
+  padding: 20px;
+`;
+
+const Heading = styled.h2`
+  color: #333;
+`;
+
+const LinkWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+const StyledLink = styled.a`
+  display: block;
+  width: 150px;
+  margin: 10px 0;
+  padding: 10px;
+  text-align: center;
+  border: 2px solid #333;
+  border-radius: 5px;
+  color: #333;
+  font-size: 1.2rem;
+  font-weight: bold;
+  font-family: italic;
+  cursor: pointer;
+  &:hover {
+    background-color: darkgreen;
+    color: #fff;
+  }
+`;
--- pages/vegetables/index.js
+++ pages/vegetables/index.js
@@ -2,13 +2,13 @@ import Header from "@/components/Header";
 import Link from "next/link";
 import { data } from "@/lib/data";
 
-export default function Fruits() {
+export default function Vegetables() {
   const currentDate = new Date();
   const currentMonth = currentDate.getMonth() + 1;
 
-  const availableFruits = data.filter((item) => {
+  const availableVegetables = data.filter((item) => {
     return (
-      item.category === "Fruit" &&
+      item.category === "Vegetable" &&
       currentMonth >= item.month_start &&
       currentMonth <= item.month_end
     );
@@ -17,9 +17,9 @@ export default function Fruits() {
   return (
     <>
       <Header />
-      <h2>Available Fruits</h2>
+      <h2>Available Vegetables</h2>
       <ul>
-        {availableFruits.map((item) => (
+        {availableVegetables.map((item) => (
           <li key={item.id}>{item.name}</li>
         ))}
       </ul>
