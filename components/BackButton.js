f978eaf280b7bab61ac0c5cb9ec3f051214924df
ArianeDahl <ariane.dahl@googlemail.com>
2023-03-13 16:24:12 +0100

    add styling

--- components/BackButton.js
+++ components/BackButton.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
+import { useRouter } from "next/router";
 
 export default function BackButton() {
+  const router = useRouter();
   return (
     <>
       <Button onClick={() => router.back()}>Back</Button>
@@ -8,4 +10,18 @@ export default function BackButton() {
   );
 }
 
-const Button = styled.button``;
+const Button = styled.button`
+  background-color: #ffffff;
+  border: 2px solid #000000;
+  border-radius: 5px;
+  color: #000000;
+  cursor: pointer;
+  font-size: 16px;
+  padding: 8px 16px;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    background-color: darkgreen;
+    color: #ffffff;
+  }
+`;
